5536e9c61a6a4f62aa767171894cbd5f321aa94f
kimmy100b <kimmy100b@gmail.com>
2022-01-11 17:47:35 +0900

    [#5] i18n 설정

--- src/main.js
+++ src/main.js
@@ -4,12 +4,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import translations from './assets/translations/index.js'
+import i18n from './assets/translations/index.js'
 
 Vue.config.productionTip = false
 new Vue({
   el: '#app',
-  translations,
+  i18n,
   router,
   store,
   render: h => h(App),
