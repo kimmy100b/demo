6c4a40846b18881828d456e2157a14454b180489
kimmy100b <kimmy100b@gmail.com>
2022-01-11 16:49:30 +0900

    [#1] 폴더 구성

--- src/main.js
+++ src/main.js
@@ -1,12 +1,25 @@
+'use strict';
+
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import translations from './assets/translations/index.js'
 
 Vue.config.productionTip = false
-
 new Vue({
+  el: '#app',
+  translations,
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: h => h(App),
+  mounted() {
+    window.addEventListener('load', this.onWindowLoad);
+  },
+  methods: {
+    onWindowLoad() {
+      // this.$router.history.current.name
+      return '#app';
+    }
+  }
+});
--- vue.config.js
+++ vue.config.js
@@ -6,8 +6,8 @@ module.exports = {
   // },
   // 옵션 체이닝
   configureWebpack: () => { // config // 웹팩 파일 보기
+    console.log('MODE >> ', process.env.NODE_ENV);
     if (process.env.NODE_ENV === 'prod' || process.env.NODE_ENV === 'dev') {
-      // console.log('MODE >> ', process.env.NODE_ENV);
       return require(`./config/webpack.${process.env.NODE_ENV}.js`);
     } else {
       console.log('Wrong webpack mode. Please choices: dev or prod.')
